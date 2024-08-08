bbba557aedf215e553b202dce40fcea1ad7b8e30
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-08-08 10:20:01 +0530

    Created a Repository folder and added a class and an interface.

---
+++ ProductApi/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Repository
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public bool AddProduct(Product product);
+        public bool UpdateProduct(int id, Product product);
+        public bool DeleteProduct(int id);
+    }
+}
