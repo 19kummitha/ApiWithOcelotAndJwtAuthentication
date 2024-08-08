b73f36de848cbc349fcea312d9e5ea99c2420a46
Ullas.HU <ullas.hu@ust.com>
2024-08-08 10:11:03 +0530

    Data folder is added and AuthenticationDbContext is added in it.

---
+++ AuthenticationWebApi/Data/AuthenticationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationWebApi.Data
+{
+    public class AuthenticationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+        {
+        }
+    }
+}
